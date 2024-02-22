3968a1453c130b772de9d9df2a0b79cca818df8f
amyrmdhn <amyramadhan99@gmail.com>
2024-02-22 14:00:14 +0700

    Add todo model

---
+++ lib/models/todo.dart
@@ -0,0 +1,13 @@
+class Todo {
+  Todo({
+    required this.id,
+    required this.title,
+    required this.detail,
+    required this.date,
+  });
+
+  final String id;
+  final String title;
+  final String detail;
+  final DateTime date;
+}
